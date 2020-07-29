79f72698f51f668d8bab0ea387691b60b113aa64
madoka0215 <feg1vjg.b@gmail.com>
2020-07-29 20:07:55 +0900

    viewとcontrollerの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'signout' => 'devise/sessions#destroy'
+  end
+
   root to: 'toppages#index'
+
+  resources :users
 end
